0c28b3e98f0a4301dd6c6c6db57243b038661a18
Plundration <matouinhell@gmail.com>
2022-12-03 17:35:03 +0100

    fjasilfjasdl

--- src/routes/mapa/distance/+page.svelte
+++ src/routes/mapa/distance/+page.svelte
@@ -1,17 +1,25 @@
-<script>
-import { onMount, onDestroy } from 'svelte';
-  onMount(async() => {
-    const settings = {
-      method: "POST",
-      headers: {
-          Accept: "aplication/json",
-          "Content-Type": "aplication/json"
-      },
-      body: JSON.stringify({lat: 0, lng: 0})
-      }
-    const response = await fetch('/mapa/distance', settings);
-    const data = await response.json();
-    console.log(data)
-  }
-  )
-  </script>
+<script lang="ts">
+    import { onMount, onDestroy } from 'svelte';
+    onMount(async () => {
+        const settings: RequestInit = {
+            method: 'POST',
+            headers: {
+                Accept: 'aplication/json',
+                'Content-Type': 'aplication/json',
+            },
+            // body: JSON.stringify({ lat: 0, lng: 0 }),
+        };
+        const response = await fetch('/mapa/distance', {
+            method: 'POST',
+            headers: {
+                Accept: 'aplication/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                lat: 0, lng: 0 
+            })
+        });
+        const data = await response.json();
+        console.log(data);
+    });
+</script>
--- src/routes/mapa/distance/+server.ts
+++ src/routes/mapa/distance/+server.ts
@@ -1,6 +1,7 @@
 import { type RequestHandler, json } from '@sveltejs/kit';
 
 export const POST: RequestHandler = async ({ request }) => {
-    const d = request.body;
-    return json({ data: d });
+    const data = await request.json();
+    console.log(data);
+    return json({ data: data });
 };
