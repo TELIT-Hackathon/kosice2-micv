ecdc7cc18748d521453780f4bd7a247f21c6109f
Plundration <matouinhell@gmail.com>
2022-12-04 05:07:12 +0100

    Push

--- src/routes/mapa/amenities/+page.svelte
+++ src/routes/mapa/amenities/+page.svelte
@@ -9,7 +9,7 @@
             },
             body: JSON.stringify({
                 center: { lat: 48.705878, lng: 21.264437 },
-                datasets: [{ name: 'zakladne_skoly', count: 3 }, { name: 'zastavky', count: 4 }, { name: 'materske_skoly', count: 2 }, { name: 'stredne_skoly', count: 2 }],
+                datasets: [{ name: 'zakladne_skoly', count: 3 }, { name: 'zastavky', count: 4 }, { name: 'materske_skoly', count: 2 }, { name: 'stredne_skoly', count: 2 }, { name: 'lekarne', count: 2 }, { name: 'psy', count: 2 }, { name: 'ambulancie', count: 2 }],
             }),
         });
         const data = await response.json();
